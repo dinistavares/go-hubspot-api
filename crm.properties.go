3b60337b1c86b949ead964bdcd65ed2d8ce9dd08
dinistavares <dinis@crisp.chat>
2024-07-23 14:52:48 +0100

    Get property

--- crm.properties.go
+++ crm.properties.go
@@ -14,30 +14,52 @@ type ObjectPropertiesResponse struct {
 }
 
 type ObjectProperty struct {
-	UpdatedAt            string      `json:"updatedAt,omitempty"`
-	CreatedAt            string      `json:"createdAt,omitempty"`
-	Name                 string      `json:"name,omitempty"`
-	Label                string      `json:"label,omitempty"`
-	Type                 string      `json:"type,omitempty"`
-	FieldType            string      `json:"fieldType,omitempty"`
-	Description          string      `json:"description,omitempty"`
-	GroupName            string      `json:"groupName,omitempty"`
-	Options              []any       `json:"options,omitempty"`
-	DisplayOrder         int         `json:"displayOrder,omitempty"`
-	Calculated           bool        `json:"calculated,omitempty"`
-	ExternalOptions      bool        `json:"externalOptions,omitempty"`
-	HasUniqueValue       bool        `json:"hasUniqueValue,omitempty"`
-	Hidden               bool        `json:"hidden,omitempty"`
-	HubspotDefined       bool        `json:"hubspotDefined,omitempty"`
-	ShowCurrencySymbol   bool        `json:"showCurrencySymbol,omitempty"`
-	ModificationMetadata interface{} `json:"modificationMetadata,omitempty"`
-	FormField            bool        `json:"formField,omitempty"`
-	CalculationFormula   string      `json:"calculationFormula,omitempty"`
-	DataSensitivity      string      `json:"dataSensitivity,omitempty"`
-	ReferencedObjectType string      `json:"referencedObjectType,omitempty"`
-	Archived             bool        `json:"archived,omitempty"`
-	CreatedUserID        string      `json:"createdUserId,omitempty"`
-	UpdatedUserID        string      `json:"updatedUserId,omitempty"`
+	UpdatedAt            string                `json:"updatedAt,omitempty"`
+	CreatedAt            string                `json:"createdAt,omitempty"`
+	Name                 string                `json:"name,omitempty"`
+	Label                string                `json:"label,omitempty"`
+	Type                 string                `json:"type,omitempty"`
+	FieldType            string                `json:"fieldType,omitempty"`
+	Description          string                `json:"description,omitempty"`
+	GroupName            string                `json:"groupName,omitempty"`
+	Options              []any                 `json:"options,omitempty"`
+	DisplayOrder         int                   `json:"displayOrder,omitempty"`
+	Calculated           bool                  `json:"calculated,omitempty"`
+	ExternalOptions      bool                  `json:"externalOptions,omitempty"`
+	HasUniqueValue       bool                  `json:"hasUniqueValue,omitempty"`
+	Hidden               bool                  `json:"hidden,omitempty"`
+	HubspotDefined       bool                  `json:"hubspotDefined,omitempty"`
+	ShowCurrencySymbol   bool                  `json:"showCurrencySymbol,omitempty"`
+	FormField            bool                  `json:"formField,omitempty"`
+	CalculationFormula   string                `json:"calculationFormula,omitempty"`
+	DataSensitivity      string                `json:"dataSensitivity,omitempty"`
+	ReferencedObjectType string                `json:"referencedObjectType,omitempty"`
+	Archived             bool                  `json:"archived,omitempty"`
+	CreatedUserID        string                `json:"createdUserId,omitempty"`
+	UpdatedUserID        string                `json:"updatedUserId,omitempty"`
+	ModificationMetadata *ModificationMetadata `json:"modificationMetadata,omitempty"`
+}
+
+type ModificationMetadata struct {
+	Archivable         bool `json:"archivable"`
+	ReadOnlyDefinition bool `json:"readOnlyDefinition"`
+	ReadOnlyValue      bool `json:"readOnlyValue"`
+}
+
+// Get a proptery by property type and name.
+func (service *PropertiesService) Get(objectType ObjectType, name string, opts *QueryValues) (*ObjectProperty, *Response, error) {
+	_url := fmt.Sprintf("/crm/%s/properties/%s/%s", *service.revision, objectType, name)
+
+	req, _ := service.client.NewRequest("GET", _url, opts, nil)
+
+	accounts := new(ObjectProperty)
+	response, err := service.client.Do(req, accounts)
+
+	if err != nil {
+		return nil, response, err
+	}
+
+	return accounts, response, nil
 }
 
 // List properties by property type.
